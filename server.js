ff9b1b0d9b2de7122611e7aa80722279565a9ef0
kayleen <8iscogb9@anonaddy.me>
2022-04-07 11:25:24 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.use(cors())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '47611782c30e47db960bc80520ab333c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/api/students', (req, res) => {
     res.status(200).send(students)
+    rollbar.log('Someone requested the student list.')
 })
 
 app.post('/api/students', (req, res) => {
@@ -33,8 +45,10 @@ app.post('/api/students', (req, res) => {
        } else {
            res.status(400).send('That student already exists.')
        }
+       rollbar.info('Someone added a student')
    } catch (err) {
        console.log(err)
+       rollbar.error(`${err} triggered in the post request to api/students`)
    }
 })
 
@@ -43,6 +57,7 @@ app.delete('/api/students/:index', (req, res) => {
     
     students.splice(targetIndex, 1)
     res.status(200).send(students)
+    rollbar.log('Someone deleted a student')
 })
 
 const port = process.env.PORT || 5050
